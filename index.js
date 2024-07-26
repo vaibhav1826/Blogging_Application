af56e8c71e03040c77276860188f84c908e17ebf
Vaibhav bhatt <148764896+vaibhav1826@users.noreply.github.com>
2024-07-26 12:19:46 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('require');
+const path =require('path');
+const express = require('express');
 const app = express(); 
 const PORT = 5000;
 
+app.set('view engine','ejs');
+app.set('views',path.resolve('./views'));
 
-
+app.get('',(req,res)=>{
+res.render('home');
+})
 // server on localhost:5000 
 app.listen(PORT,()=>{
 console.log(`server starting on port : ${PORT}`);
