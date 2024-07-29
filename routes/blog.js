2ec4ede3ea1815b02328d58abb0b1717afe67758
Vaibhav bhatt <148764896+vaibhav1826@users.noreply.github.com>
2024-07-29 21:39:17 +0530

    adding blogs routes

--- routes/blog.js
+++ routes/blog.js
@@ -4,7 +4,12 @@ const route = Router();
 route.get("/add-new",(req,res)=>{
     return res.render('addBlogs',{
         user : req.user,
-    })
+    });
 });
+
+route.post("/",(req,res)=>{
+        console.log(req.body);
+        return res.redirect("/");
+    });
 
 module.exports = route;
