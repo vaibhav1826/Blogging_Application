dfc5572ee06fdcc3ef2a79538d70196db35e1d86
Vaibhav bhatt <148764896+vaibhav1826@users.noreply.github.com>
2024-07-30 14:58:47 +0530

    final commmit

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-const path =require('path');
-const express = require('express');
+const path =require('path');  // importing path module 
+const express = require('express'); // importing express module
+
+// importing mongoose for connection with database
 const mongoose = require('mongoose');
+
+// importing cookie-parser module for access
 const cookieparser = require('cookie-parser');
+
+//importing blog file from models/blog.js
 const Blog = require('./models/blog.js');
 
 const { checkforauthenticationcookie } = require('./middleware/authenthication.js'); // importing middleware
 
 const app = express(); 
+
 const userRoute = require('./routes/user.js');
-const blogRoute = require('./routes/blog.js');
+ // for redirecting into the user.js
+
+ const blogRoute = require('./routes/blog.js');
+ // for redirecting into the blog.js
+
 const PORT = 5000;
 
+// connecting database -- mongodb
 mongoose
 .connect("mongodb://localhost:27017/blogging_application")
 .then((e)=>console.log(`mongodb connected`));
 
+// setting the view engine as ejs 
 app.set('view engine','ejs');
 app.set('views',path.resolve('./views'));
 
+// importing middleware 
 app.use(express.urlencoded({ extended :false }));
 app.use(cookieparser());
 app.use(checkforauthenticationcookie("token"));
 app.use(express.static(path.resolve("./public")));
 
+// redirecting it to home page 
 app.get('/',async(req,res)=>{
     const allBlogs = await Blog.find({});
     res.render("home", {
@@ -30,6 +45,7 @@ app.get('/',async(req,res)=>{
       blogs: allBlogs,
     });
 });
+
 
 app.use('/user',userRoute);
 app.use('/blog',blogRoute);
